2c8164f7a0449bd957a6330e862a3b7ad56c3195
C1fer <41005657+C1fer@users.noreply.github.com>
2023-04-18 04:49:32 -0400

    Cambio de favicon y logo en pagina principal

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,8 +8,8 @@ export const Navbar = ({ darkTheme, setDarkTheme }) => {
     <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
         <div className='flex justify-between items-center space-x-5 w-screen'>
             <Link to="/">
-              <p className='text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900'>
-                Seapedia🔍
+              <p className='text-2xl bg-[#7b7de7] font-bold text-white py-1 px-2 rounded  dark:text-slate-200'>
+                Seapedia
               </p>
             </Link>
             <button type="button" onClick={() => setDarkTheme(!darkTheme)} className='text-xl dark:bg-slate-700 dark:text-slate-200 bg-white border-1 rounded-full px-2 py-1 hover:shadow-lg'>
