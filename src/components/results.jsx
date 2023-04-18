78e6619340702ae9702dc59f3c4a162c27522d9e
C1fer <41005657+C1fer@users.noreply.github.com>
2023-04-18 16:18:42 -0400

    Fixed design of News and Images Sections

--- src/components/results.jsx
+++ src/components/results.jsx
@@ -7,7 +7,7 @@ import { Loading } from './loading';
 export const Results = () => {
   const { results, isLoading, getResults, searchTerm } = useResultContext();
   const location = useLocation();
-  const apikey = '643ec04eb53405e9ff0f3386';
+  const apikey = '643ef9dcb53405e9ff0f3387';
 
   useEffect(() => {
 
@@ -48,8 +48,8 @@ export const Results = () => {
         <div className='flex flex-wrap justify-center items-center'>
           {results?.image_results?.map(({ link, title, original }, index)=>(
             <a href={link} target="_blank" key={index} rel="noreferrer" className="sm:p-3 p-5">
-            <img src={original} alt={title} loading="lazy" className="h-auto w-auto max-h-64 max-w-64" />
-            <p className="sm:w-36 w-64 break-words text-sm mt-2">{title}</p>
+            <img src={original} alt={title} loading="lazy" className=" object-cover h-96 w-96" />
+            <p className="sm:w-[23rem] w-[23rem] truncate text-base mt-2">{title}</p>
           </a>
         ))}
       </div>
@@ -57,25 +57,26 @@ export const Results = () => {
     case '/news':
       return (
         <div className="sm:px-56 flex flex-wrap justify-between items-center space-y-6">
-          {results?.news_results?.map(({title, source, snippet, url }, index) => (
+          {results?.news_results?.map(({title, source, snippet, url, lastUpdated, imgSrc }, index) => (
             <div key={index} className="md:w-2/5 w-full ">
-              
-                <p className="text-lg dark:text-blue-300 text-blue-700">
+              <p>
+                {source}
+              </p>        
+              <a href={url} target="_blank" rel="noreferrer">
+                <p className="text-lg font-semibold hover:underline dark:text-blue-300 text-blue-700">
                   {title}
                 </p>
-                <p>
-                  {snippet}
-                </p>
-              <div className="flex gap-4">
-                <p>
-                  {source}
-                </p>  
-                <p>
-                  {url}
-                </p>             
-                </div>
+              </a>
+              <p>
+                {snippet}
+              </p>
+              <img src={imgSrc} alt={title} loading="lazy" className=" object-cover h-[92px] w-[92px]" />
+              <p className='mt-1 text-sm text-slate-500'>
+                  {lastUpdated}
+              </p>  
             </div>
           ))}
+
         </div>
       );
     case '/videos':
@@ -85,7 +86,7 @@ export const Results = () => {
             <div key={index} className="md:2/5 w-full m-1">
               <a href={link} target="_blank" rel="noreferrer">
                 <p>{displayed_link}</p>
-                <p className='text-lg hover:underline dark:text-blue-300 text-blue-700'>{title}</p>
+                <p className='text-lg font-semibold hover:underline dark:text-blue-300 text-blue-700 truncate ...'>{title}</p>
               </a>
               <ReactPlayer url={link}  controls width="355px" height="200px" />
             </div>
