da45b9f55f7f28515921ae2a0b1c9af8f4f69936
EliardoB <1086299@est.intec.edu.do>
2023-04-18 02:46:01 -0400

    Funcionalidad hecha

--- src/components/results.jsx
+++ src/components/results.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 
@@ -8,10 +8,18 @@ import { Loading } from './loading';
 export const Results = () => {
   const { results, isLoading, getResults, searchTerm } = useResultContext();
   const location = useLocation();
+  const apikey = '643e219ffa3387e9f86288bf&q';
+
 
   useEffect(() => {
-    getResults('/search/Javascript Mastery')
-  }, [])
+
+    if (searchTerm !== '') {
+        getResults(`${location.pathname}?api_key=${apikey}=${searchTerm}`);
+      }
+    
+    
+  }, [searchTerm, location.pathname]);
+
 
   if(isLoading) return <Loading />;
 
@@ -19,26 +27,69 @@ export const Results = () => {
     case '/search':
       return (
         <div className='flex flex-wrap justify between space-y-6 sm:px-56'>
-          {results?.results?.map(({ link, title }, index)=>(
+          {results?.organic_results?.map(({ link, title, snippets }, index)=>(
             <div key={index} className='md:2/5 w-full'>
               <a href={link} target="_blank" rel="noreferrer">
                 <p className='text-sm'>
-                  {link.length > 30 ? link.substring(0, 30) : link}
+                  {link}
                 </p>
                 <p className='text-lg hover:underline dark:text-blue-300 text-blue-700'>
                   {title}
+                </p>
+                <p>
+                  {snippets}
                 </p>
               </a>
             </div>
           ))}
         </div>
-      )
+      );
     case '/images':
-      return 'SEARCH';
+      return (
+        <div className='flex flex-wrap justify-center items-center'>
+          {results?.image_results?.map(({ link, title, original }, index)=>(
+            <a href={link} target="_blank" key={index} rel="noreferrer" className="sm:p-3 p-5">
+            <img src={original} alt={title} loading="lazy" />
+            <p className="sm:w-36 w-36 break-words text-sm mt-2">{title}</p>
+          </a>
+        ))}
+      </div>
+    );
     case '/news':
-      return 'SEARCH';
+      return (
+        <div className="sm:px-56 flex flex-wrap justify-between items-center space-y-6">
+          {results?.news_results?.map(({title, source, snippet }, index) => (
+            <div key={index} className="md:w-2/5 w-full ">
+              
+                <p className="text-lg dark:text-blue-300 text-blue-700">
+                  {title}
+                </p>
+                <p>
+                  {snippet}
+                </p>
+              <div className="flex gap-4">
+                <p>
+                  {source}
+                </p>              
+                </div>
+            </div>
+          ))}
+        </div>
+      );
     case '/videos':
-      return 'SEARCH';
+      return (
+        <div className="flex flex-wrap ">
+          {results?.video_results?.map(({link, title, displayed_link}, index) => (
+            <div key={index} className="p-2">
+              <a href={link} target="_blank" rel="noreferrer">
+                <p>{displayed_link}</p>
+                <p className='text-lg hover:underline dark:text-blue-300 text-blue-700'>{title}</p>
+              </a>
+              <ReactPlayer url={link}  controls width="355px" height="200px" />
+            </div>
+          ))}
+        </div>
+      );
     default:
       return 'ERROR';
   }
--- src/context/ResultContextProvider.js
+++ src/context/ResultContextProvider.js
@@ -1,23 +1,20 @@
 import React, {createContext, useContext, useState} from "react";
 
 const ResultContext = createContext();
-const baseUrl = 'https://google-search74.p.rapidapi.com/';
+const baseUrl = 'https://api.serpdog.io';
+
 
 export const ResultContextProvider = ({ children }) => {
     const [results, setResults] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [searchTerm, setSearchTerm] = useState('');
+    const [searchTerm, setSearchTerm] = useState('Gatos');
 
     // get videos, images...
     const getResults = async (type) => {
         setIsLoading(true);
 
         const response = await fetch(`${baseUrl}${type}`, {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': 'ad9744692amsh12b9c9f364fbce6p155eb1jsn56b3c9a9ad90',
-                'X-RapidAPI-Host': 'google-search74.p.rapidapi.com'
-              }
+            method: 'GET'
 
         });
         
