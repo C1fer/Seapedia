4a1f615afb27b3147274cea5bb252d83ef4b112a
C1fer <cifer037@gmail.com>
2023-08-11 01:42:00 -0400

    Update results.jsx

--- src/components/results.jsx
+++ src/components/results.jsx
@@ -7,7 +7,7 @@ import { Loading } from './loading';
 export const Results = () => {
   const { results, isLoading, getResults, searchTerm } = useResultContext();
   const location = useLocation();
-  const apikey = '6440e4543b4183a06b1e3a79';
+  const apikey = process.env.API_KEY;
 
   useEffect(() => {
 
